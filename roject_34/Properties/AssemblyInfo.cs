b5dcc6a9cf485bbb9e46747b1993db481a36bdfb
Olga Nazarova <mazon2010@yandex.ru>
2022-02-15 12:34:46 +0300

    correct

--- roject_34/Properties/AssemblyInfo.cs
+++ roject_34/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("project")]
+[assembly: AssemblyTitle("roject_34")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("project")]
+[assembly: AssemblyProduct("roject_34")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("af03d358-5432-4d8b-b4cd-e82470108c0a")]
+[assembly: Guid("de2a32bd-3782-4821-8763-a2b12ac1e0ac")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
